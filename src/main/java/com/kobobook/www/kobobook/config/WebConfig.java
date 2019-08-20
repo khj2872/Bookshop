cc5b128c1047fb7e24eabaa301839c7adb772c3a
khj2872 <khj2872@daum.net>
2019-08-20 16:28:01 +0900

    test

--- src/main/java/com/kobobook/www/kobobook/config/WebConfig.java
+++ src/main/java/com/kobobook/www/kobobook/config/WebConfig.java
@@ -1,7 +1,10 @@
 package com.kobobook.www.kobobook.config;
 
+import com.kobobook.www.kobobook.filter.AccessLogingFilter;
 import com.kobobook.www.kobobook.interceptor.JwtInterceptor;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,11 +21,21 @@ public class WebConfig implements WebMvcConfigurer {
 
     private JwtInterceptor jwtInterceptor;
 
+    private AccessLogingFilter accessLogingFilter;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor)
                 .addPathPatterns("/api/**")
                 .excludePathPatterns(EXCLUDE_PATHS);
+    }
+
+    @Bean
+    public FilterRegistrationBean<AccessLogingFilter> filterRegistrationBean() {
+        FilterRegistrationBean<AccessLogingFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(accessLogingFilter);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
     }
 
 }
--- src/main/java/com/kobobook/www/kobobook/filter/AccessLogingFilter.java
+++ src/main/java/com/kobobook/www/kobobook/filter/AccessLogingFilter.java
@@ -1,5 +1,6 @@
 package com.kobobook.www.kobobook.filter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -7,13 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class AccessLogingFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final String url = ((HttpServletRequest) req).getRequestURI();
 
-        if(url.matches("/(profile|actuator)")) {
+        if(url.equals("/profile") || url.equals("/actuator/health") || url.equals("/favicon.ico")) {
             req.setAttribute("ignoreLogging", true);
         }
 
